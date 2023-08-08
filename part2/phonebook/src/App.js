2d05f43cdbc0d90bdb16eb89e72c84de970192b6
Dragos Solca <dcsolca@gmail.com>
2023-08-08 17:08:06 +0300

    Refactoring + adding styles

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,21 @@
 import { useEffect, useState } from 'react'
 import Persons from './components/Persons.js'
 import PersonForm from './components/PersonForm.js'
-import axios from 'axios'
+import Notification from './components/Notification.js'
+
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -24,19 +27,62 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    if(persons.filter(person => person.name === newName).length === 0) {
-      const personObject = {
-        name: newName,
-        number: newNumber,
-        id: persons.length + 1,
-      }
-      setPersons(persons.concat(personObject))
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
     }
-    else{
-      window.alert(`${newName} is already added to phonebook`)
+
+    const foundPersons = persons.filter(person => person.name === newName)
+    if (foundPersons.length === 0) {
+      personService
+        .create(personObject)
+        .then(() => {
+          setPersons(persons.concat(personObject))
+
+          setErrorMessage(
+            `Added ${personObject.name}`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000) 
+          
+        })
     }
-    setNewName('')
-    setNewNumber('')
+
+    else {
+      window.alert(`${newName} is already added to phonebook, replace the old number with a new one?`)
+
+      console.log(foundPersons)
+      personService
+        .update(foundPersons[0].id, personObject)
+        .then((returnedPerson) => {
+          const updatedPersons = persons.map(person => person.id !== personObject.id ? person : returnedPerson)
+          setPersons(updatedPersons)
+
+          setErrorMessage(
+            `Updated ${personObject.name}`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          .catch(error => {
+            alert(
+              `The person '${personObject.name}' was already deleted from server`
+            )
+            setPersons(persons.filter(person => person.id !== personObject.id))
+          })
+        })
+
+      personService
+        .getAll()
+        .then(response => {
+          setPersons(response.data)
+        })
+    }
+    setNewName("")
+    setNewNumber("")
+
   }
 
   const handleNameChange = (event) => {
@@ -52,25 +98,45 @@ const App = () => {
     setShowAll(false)
   }
 
+  const deletePerson = (id, name) => {
+    if (window.confirm("Delete " + name)) {
+      const person = persons.find(person => person.id === id)
+      personService
+        .destroy(id, person)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+        .catch(error => {
+          alert(
+            `the person '${person.name}' was already deleted from server`
+          )
+          setPersons(persons.filter(person => person.id !== id))
+        })
+    }
+
+
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <div>
-          filter shown with <input value={newFilter} onChange={handleFilterChange} />
-        </div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
       <h2>add a new</h2>
-      <PersonForm submitFunction={addPerson} 
-                  fields={[ 
-                            {value: {newName}, onChange: {handleNameChange}}, 
-                            {value: {newNumber}, onChange: {handleNumberChange}}
-                          ]}
+      <PersonForm submitFunction={addPerson}
+        fields={[
+          { value: { newName }, onChange: { handleNameChange } },
+          { value: { newNumber }, onChange: { handleNumberChange } }
+        ]}
       />
 
       <h2>Numbers</h2>
-      <Persons filteredPersons={filteredPersons}/>
-        
-      
+      <Persons filteredPersons={filteredPersons} onDelete={deletePerson} />
+
+
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,12 +1,16 @@
-import Person from './Person'
-
-const Persons = ({filteredPersons}) => {
+const Persons = ({filteredPersons, onDelete}) => {
     return (
         <>   
           {filteredPersons.map(person => 
-            <Person key={person.id} person={person} />
+            <div key = {person.id}>
+              {person.name} {person.number} {' '}
+              <button onClick={() => onDelete(person.id, person.name)}>delete</button>
+            </div>
           )}
-        </>
+       </>     
+            
+          
+        
     )
 }
 
